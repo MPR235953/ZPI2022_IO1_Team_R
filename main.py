4313db017c40b58dff702d0e0d3daf93888cca09
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-03-22 09:48:38 +0100

    feat: implemented division function

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def division(a: float, b: float) -> float:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("ZeroDivisionError :(")
+
 
 if __name__ == '__main__':
     print("Team R\nMPR235953\nThePuccaPl\nvor-o-na")
