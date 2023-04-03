7c734db1ee60d71e0706b4f9dcb1e69d53fc7184
ThePuccaPl <thepuccapl@gmail.com>
2023-04-03 18:08:11 +0200

    added comment

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def division(a: float, b: float) -> float:
         return a / b
     except ZeroDivisionError:
         print("ZeroDivisionError :(")
-        
+
 def mult(a, b):
     return a*b
 
 def difference(a, b):
     return a-b
 
+# returns the sum of two numbers
 def addition(a, b):
     return a+b
 
