21269505495c8d3cd3240398bf10cad67237f4a0
ThePuccaPl <thepuccapl@gmail.com>
2023-05-08 13:26:13 +0200

    implemented functionality to get data from past week

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import datetime
+import requests
+
+USD_CODE = 'USD'
+EUR_CODE = 'EUR'
 
 
 def get_today():
@@ -8,6 +12,21 @@ def get_today():
 def get_date(offset):
     date = datetime.datetime.today() - datetime.timedelta(days=offset)
     return date.strftime('%Y-%m-%d')
+
+
+def extract_data(json):
+    rates = json['rates']
+    data = []
+    for rate in rates:
+        data.append(rate['mid'])
+    return data
+
+
+def get_week(code):
+    url = 'http://api.nbp.pl/api/exchangerates/rates/A/' + code + '/' + get_date(7) + '/' + get_today() + '/'
+    response = requests.get(url)
+    data = extract_data(response.json())
+    return data
 
 
 if __name__ == '__main__':
