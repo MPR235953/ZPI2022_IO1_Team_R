bdd9ff3e2b8b12c41ef56ad61b3d37718496b601
ThePuccaPl <thepuccapl@gmail.com>
2023-05-08 13:24:28 +0200

    implemented functionality to get current date and past date given the offset

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
+import datetime
+
+
+def get_today():
+    return datetime.datetime.today().strftime('%Y-%m-%d')
+
+
+def get_date(offset):
+    date = datetime.datetime.today() - datetime.timedelta(days=offset)
+    return date.strftime('%Y-%m-%d')
+
 
 if __name__ == '__main__':
     pass
